bbfc0c0447d67d9d1c5d138a70743bc627e8d0b4
rysk1013 <rysk2908@gmail.com>
2021-04-25 03:16:30 +0900

    posts/createアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:text).merge(user_id: current_user.id)
   end
 end
