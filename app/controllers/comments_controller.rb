5c9fd4554bb21d7aa6cea2a9970868d7e718ab88
rysk1013 <rysk2908@gmail.com>
2021-05-01 02:35:34 +0900

    comments/createアクションの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,8 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
-    if @comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      render "posts/show"
-    end
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
   end
 
   private
