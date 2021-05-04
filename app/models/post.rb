eae76f45ba35577cc27c2b34092d3de01118f873
rysk1013 <rysk2908@gmail.com>
2021-05-04 18:31:59 +0900

    メソッドの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,27 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :notification, dependent: :destroy
   validates :text, presence: true
+
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(:post_id: id).where.not(:user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      post_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+
+    notification.save if notification.valid?
+  end
 end
