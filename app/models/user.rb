81f777624417a94d69fc135d94cdbf80c7e2ddd7
rysk1013 <rysk2908@gmail.com>
2021-05-05 19:21:08 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
   has_many :active_notifications, class_name: 'notification', foreign_key: 'visitor_id', dependent: :destroy
-  has_many :passive_notifications, class_name: 'notification', foreign_key: 'visited_id', dependetn: :destroy
+  has_many :passive_notifications, class_name: 'notification', foreign_key: 'visited_id', dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
