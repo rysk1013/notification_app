6db28db99a1ef58590ba8ea34399d780ee47066c
rysk1013 <rysk2908@gmail.com>
2021-05-07 01:57:46 +0900

    notifications/resetアクションの設定

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,18 @@
 class NotificationsController < ApplicationController
+  before_action :set_notifications
+
   def index
-    @notifications = current_user.passive_notifications
+  end
+
+  def reset
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
+    redirect_to notifications_path
+  end
+
+  private
+  def set_notifications
+    @notifications = current_user.passive_notifications
   end
 end
