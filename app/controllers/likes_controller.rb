97412f75011d28cdb7b8f09960a37c375604679a
rysk1013 <rysk2908@gmail.com>
2021-05-08 03:37:21 +0900

    likes/createアクションの追加設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,8 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(post_id: params[:post_id], user_id: current_user.id)
+    post = Post.find(params[:post_id])
+    post.create_notification_like!(current_user)
     redirect_back(fallback_location: root_path)
   end
 
