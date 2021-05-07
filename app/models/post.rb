3dd3c7fe2f4ac3eee0c253d45057807741875e95
rysk1013 <rysk2908@gmail.com>
2021-05-08 03:36:53 +0900

    メソッドの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,6 +14,24 @@ class Post < ApplicationRecord
     save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
   end
 
+  def create_notification_like!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ?,", current_user.id, user_id, id, 'like'])
+    
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+
+      notification.save if notification.valid?
+    end
+  end
+
   def save_notification_comment!(current_user, comment_id, visited_id)
     notification = current_user.active_notifications.new(
       post_id: id,
